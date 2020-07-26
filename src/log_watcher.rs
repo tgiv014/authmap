d439a454d1069b138b375ed1da4b8acb176823f7
Thomas Gorham <tgiv014@gmail.com>
2020-07-26 13:31:39 -0500

    Put together basic functionality
    This commit adds everything necessary to monitor ssh logs,
    extract desired info from them, and push them into influxdb.
    
    TODO:
    - Read a configuration file for db auth, etc.
    - Dockerize

---
+++ src/log_watcher.rs
@@ -0,0 +1,116 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::{BufReader, ErrorKind, SeekFrom};
+use std::os::unix::fs::MetadataExt;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub struct LogWatcher {
+    filename: String,
+    inode: u64,
+    pos: u64,
+    reader: BufReader<File>,
+    finish: bool,
+}
+
+impl LogWatcher {
+    pub fn register(filename: String) -> Result<LogWatcher, io::Error> {
+        // Attempt to open a handle for the file, escalate error if needed
+        let f = match File::open(filename.clone()) {
+            Ok(x) => x,
+            Err(err) => return Err(err),
+        };
+
+        let metadata = match f.metadata() {
+            Ok(x) => x,
+            Err(err) => return Err(err),
+        };
+        
+        // Make a reader and seek to the end of the file
+        let mut reader = BufReader::new(f);
+        let pos = metadata.len();
+        reader.seek(SeekFrom::Start(pos)).unwrap();
+
+        // Return success, logwatcher object
+        Ok(LogWatcher {
+            filename: filename,
+            inode: metadata.ino(),
+            pos: pos,
+            reader: reader,
+            finish: false,
+        })
+    }
+
+    fn reopen_if_log_rotated<F: ?Sized>(&mut self, callback: &F)
+    where
+        F: Fn(String),
+    {
+        loop {
+            match File::open(self.filename.clone()) {
+                Ok(x) => {
+                    let f = x;
+                    let metadata = match f.metadata() {
+                        Ok(m) => m,
+                        Err(_) => {
+                            sleep(Duration::new(1, 0));
+                            continue;
+                        }
+                    };
+                    if metadata.ino() != self.inode {
+                        self.finish = true;
+                        self.watch(callback);
+                        self.finish = false;
+                        println!("reloading log file");
+                        self.reader = BufReader::new(f);
+                        self.pos = 0;
+                        self.inode = metadata.ino();
+                    } else {
+                        sleep(Duration::new(1, 0));
+                    }
+                    break;
+                }
+                Err(err) => {
+                    if err.kind() == ErrorKind::NotFound {
+                        sleep(Duration::new(1, 0));
+                        continue;
+                    }
+                }
+            };
+        }
+    }
+
+    pub fn watch<F: ?Sized>(&mut self, callback: &F)
+    where
+        F: Fn(String),
+    {
+        loop {
+            // Read a line
+            let mut line = String::new();
+            let resp = self.reader.read_line(&mut line);
+            match resp {
+                Ok(len) => {
+                    if len > 0 {
+                        // If we read data successfully, move our position and seek ahead
+                        // Call the callback with the line we read
+                        self.pos += len as u64;
+                        self.reader.seek(SeekFrom::Start(self.pos)).unwrap();
+                        callback(line.replace("\n", ""));
+                        line.clear();
+                    } else {
+                        // We didn't read anything... we should check if the log was rotated
+                        if self.finish {
+                            break;
+                        } else {
+                            self.reopen_if_log_rotated(callback);
+                            self.reader.seek(SeekFrom::Start(self.pos)).unwrap();
+                        }
+                    }
+                }
+                Err(err) => {
+                    println!("{}", err);
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,143 @@
+use regex::Regex;
+use influent::create_client;
+use influent::client::{Client, Credentials};
+use influent::measurement::{Measurement, Value};
+use futures::Future;
+use maxminddb::geoip2;
+
+#[macro_use] extern crate lazy_static;
+
+mod log_watcher;
+use log_watcher::LogWatcher;
+
+fn is_log_from_sshd(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"sshd\[[0-9]+\]:").unwrap();
+    }
+    return RE.is_match(line);
+}
+
+fn get_logline(line: &str) -> &str {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"sshd\[[0-9]+\]: +(.+)$").unwrap();
+    }
+    let caps = RE.captures(line).unwrap();
+    return caps.get(1).unwrap().as_str();
+}
+
+fn is_log_accepted(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^Accepted.+$").unwrap();
+    }
+    return RE.is_match(line);
+}
+
+fn is_log_good_disconnect(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^Disconnected from user.+$").unwrap();
+    }
+    return RE.is_match(line);
+}
+
+fn is_log_bad_disconnect(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^Disconnected from.+$").unwrap();
+    }
+    return RE.is_match(line);
+}
+
+fn is_log_invalid_user(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^Connection closed|reset by invalid user.+$").unwrap();
+    }
+    return RE.is_match(line);
+}
+
+fn get_ipaddr(line: &str) -> &str {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})").unwrap();
+    }
+    let caps = RE.captures(line).unwrap();
+    return caps.get(1).unwrap().as_str();
+}
+
+pub struct InfluxInjector<'a> {
+    client: influent::client::http::HttpClient<'a>,
+    reader: maxminddb::Reader<Vec<u8>>,
+}
+
+impl<'a> InfluxInjector<'a> {
+    pub fn new() -> InfluxInjector<'a> {
+        let credentials = Credentials {
+            username: "root",
+            password: "root",
+            database: "db0"
+        };
+        let hosts = vec!["http://localhost:8086"];
+
+        InfluxInjector {
+            client: create_client(credentials, hosts),
+            reader: maxminddb::Reader::open_readfile("./GeoLite2-City.mmdb").unwrap(),
+        }
+    }
+
+    pub fn callback(&self, line: String) {
+        let line_str: &str = line.as_str();
+        if !is_log_from_sshd(line_str) {
+            return;
+        }
+        let logline = get_logline(line_str);
+        let tag;
+
+        if is_log_accepted(logline) {
+            tag = "accepted";
+        } else if is_log_good_disconnect(logline) {
+            tag = "good_disconnect";
+        } else if is_log_bad_disconnect(logline) {
+            tag = "bad_disconnect";
+        } else if is_log_invalid_user(logline) {
+            tag = "invalid_user";
+        } else {
+            return;
+        }
+        let ip_addr = get_ipaddr(logline);
+        println!("Got a {} hit from {}", tag, ip_addr);
+
+        let mut measurement = Measurement::new("ssh_hits");
+        measurement.add_tag("type", tag);
+        measurement.add_tag("ip", ip_addr);
+        measurement.add_field("ip_addr", Value::String(ip_addr));
+
+        match self.reader.lookup(ip_addr.parse::<std::net::IpAddr>().unwrap()) {
+            Ok(x) => {
+                let city: geoip2::City = x;
+                let location = city.location.unwrap();
+                let country = city.country.unwrap().names.unwrap()["en"];
+                measurement.add_field("latitude", Value::Float(location.latitude.unwrap()));
+                measurement.add_field("longitude", Value::Float(location.longitude.unwrap()));
+                measurement.add_field("country", Value::String(country));
+            }
+            Err(err) => {
+                println!("Failed to get a position for {} - {}", ip_addr, err);
+            }
+        }
+        
+
+        // As far as I can tell, this is the easiest way to force a blocking async run
+        // in a non async app
+        ::tokio::run(self.client.write_one(measurement, None).map_err(|e| panic!(e)));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "/var/log/auth.log".to_string();
+
+    let influx_injector = InfluxInjector::new();
+
+    // Closures are apparently the easy way to do callbacks into stateful objects
+    let callback = |line: String| influx_injector.callback(line);
+
+    let mut log_watcher = LogWatcher::register(filename).unwrap();
+
+    log_watcher.watch(&callback);
 }
